6fa96cbefdb46e12184d575e6760b0afb1b03351
bohdanm-bb <bohdan.makarenko@blackbird.io>
2023-12-14 00:48:55 +0200

    api throttle fix

--- Apps.MicrosoftExcel/MicrosoftExcelClient.cs
+++ Apps.MicrosoftExcel/MicrosoftExcelClient.cs
@@ -1,6 +1,7 @@
 ﻿using Apps.MicrosoftExcel.Dtos;
 using Apps.MicrosoftExcel.Extensions;
 using RestSharp;
+using System.Linq;
 
 namespace Apps.MicrosoftExcel;
 
@@ -26,6 +27,13 @@ public class MicrosoftExcelClient : RestClient
     public async Task<RestResponse> ExecuteWithHandling(RestRequest request)
     {
         var response = await ExecuteAsync(request);
+
+        if(response.StatusCode == System.Net.HttpStatusCode.TooManyRequests)
+        {
+            int timeout = int.Parse(response.Headers.Where(x => x.Name == "Retry-After").First().Value.ToString());
+            await Task.Delay(timeout * 1000);
+            return await ExecuteWithHandling(request);
+        }
 
         if (response.IsSuccessful)
             return response;
