c4d4ea3f5ebc47cd4c1749da0e9d1f4987116606
mathijs-bb <mathijs.sonnemans@blackbird.io>
2024-01-05 12:33:25 +0100

    Improved user experience of add and update sheet row

---
+++ Apps.MicrosoftExcel/Extensions/IntExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Apps.MicrosoftExcel.Extensions
+{
+    public static class IntExtensions
+    {
+        // https://www.geeksforgeeks.org/find-excel-column-name-given-number/
+        public static string ToExcelColumnAddress(this int columnNumber)
+        {
+            string columnName = "";
+
+            while (columnNumber > 0)
+            {
+                int rem = columnNumber % 26;
+                if (rem == 0)
+                {
+                    columnName += "Z";
+                    columnNumber = (columnNumber / 26) - 1;
+                }
+                else
+                {
+                    columnName += (char)((rem - 1) + 'A');
+                    columnNumber = columnNumber / 26;
+                }
+            }
+            return columnName.Reverse();
+        }
+    }
+}
