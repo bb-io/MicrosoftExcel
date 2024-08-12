a72cce086d1315dca7d9c8685191c99321323400
ce-nistal <cecilia.nistal@blackbird.io>
2024-08-12 12:57:42 -0300

    Added Rows count to Get range actions

--- Apps.MicrosoftExcel/Dtos/RowsDto.cs
+++ Apps.MicrosoftExcel/Dtos/RowsDto.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Blackbird.Applications.Sdk.Common;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,5 +10,8 @@ namespace Apps.MicrosoftExcel.Dtos
     public class RowsDto
     {
         public List<List<string>> Rows { get; set; }
+
+        [Display("Rows Count")]
+        public double RowsCount { get; set; }
     }
 }
