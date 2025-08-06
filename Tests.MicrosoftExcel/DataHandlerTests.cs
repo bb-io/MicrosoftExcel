f367817fcf2224f786105a8aca5c9e1017b427d1
RiabushenkoA <ariabushenko@blackbird.io>
2025-08-06 14:20:55 +0300

    Added test project and unit tests

---
+++ Tests.MicrosoftExcel/DataHandlerTests.cs
@@ -0,0 +1,24 @@
+﻿using Apps.MicrosoftExcel.DataSourceHandlers;
+using Blackbird.Applications.Sdk.Common.Dynamic;
+
+namespace Tests.MicrosoftExcel
+{
+    [TestClass]
+    public class DataHandlerTests : TestBase
+    {
+        [TestMethod]
+        public async Task WorkbookDataSourceHandler_IsSuccess()
+        {
+            var handler = new WorkbookDataSourceHandler(InvocationContext, new Apps.MicrosoftExcel.Models.Requests.WorkbookRequest { });
+
+            var response = await handler.GetDataAsync(new DataSourceContext { SearchString = "" }, CancellationToken.None);
+
+            foreach (var item in response)
+            {
+                Console.WriteLine($"Id: {item.Key}, Name: {item.Value}");
+            }
+
+            Assert.IsNotNull(response);
+        }
+    }
+}
