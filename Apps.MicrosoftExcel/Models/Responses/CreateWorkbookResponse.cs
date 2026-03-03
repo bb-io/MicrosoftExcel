d7bbe6d9e775898a0e1a34398d61f657a4a1e033
Artem Riabushenko <ariabushenko@blackbird.io>
2026-03-03 22:46:22 +0200

    Added action 'Create empty workbook'

---
+++ Apps.MicrosoftExcel/Models/Responses/CreateWorkbookResponse.cs
@@ -0,0 +1,7 @@
+﻿namespace Apps.MicrosoftExcel.Models.Responses;
+
+public class CreateWorkbookResponse
+{
+    public string WorkbookId { get; set; } = default!;
+    public string Name { get; set; } = default!;
+}
